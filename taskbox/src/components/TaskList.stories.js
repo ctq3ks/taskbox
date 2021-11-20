f85410f88cc5d0044869111b867b27c84ce5b575
ctq3ks <conortquinnirons@gmail.com>
2021-11-20 10:49:23 -0500

    PureTaskList

--- taskbox/src/components/TaskList.stories.js
+++ taskbox/src/components/TaskList.stories.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-import TaskList from './TaskList';
+import { PureTaskList } from './TaskList';
 import * as TaskStories from './Task.stories';
 
 export default {
-  component: TaskList,
+  component: PureTaskList,
   title: 'TaskList',
   decorators: [story => <div style={{ padding: '3rem' }}>{story()}</div>],
 };
 
-const Template = args => <TaskList {...args} />;
+const Template = args => <PureTaskList {...args} />;
 
 export const Default = Template.bind({});
+
 Default.args = {
   // Shaping the stories through args composition.
   // The data was inherited from the Default story in task.stories.js.
