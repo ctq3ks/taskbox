d30a50889952ee4580db9d103d6e10f5f8bdcf43
ctq3ks <conortquinnirons@gmail.com>
2021-11-24 09:00:35 -0500

    git cont'

--- taskbox/src/components/InboxScreen.js
+++ taskbox/src/components/InboxScreen.js
@@ -11,8 +11,9 @@ export function PureInboxScreen({ error }) {
       <div className="page lists-show">
         <div className="wrapper-message">
           <span className="icon-face-sad" />
-          <div className="title-message">Oh no!</div>
+          
           <div className="subtitle-message">Something went wrong</div>
+          <div className="title-message">Oh no!</div>
         </div>
       </div>
     );
